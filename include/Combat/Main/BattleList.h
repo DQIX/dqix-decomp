838c187242f7d76b542bdf1573f05de63d380149
nat5_776 <deindall@outlook.com>
2025-05-24 13:48:37 +0200

    Use bitfields rather than raw bitwise math for accessing battle stats

--- include/Combat/Main/BattleList.h
+++ include/Combat/Main/BattleList.h
@@ -1,54 +1,46 @@
 #pragma once
 
-#define BUFF_BITS_START 0x1d
-#define BUFF_BITS_ATTACK 0x1d
-#define BUFF_BITS_DEFENSE 0x1a
-#define BUFF_BITS_AGILITY 0x17
-#define BUFF_BITS_CHARM 0x14
-#define BUFF_BITS_MAGICAL_MIGHT 0x11
-#define BUFF_BITS_MAGICAL_MENDING 0xe
-
-#define STAT_BITS_START 0x16
-#define STAT_BITS_CHARM 0x16
-#define STAT_MASK_CHARM 0x3FF
-#define STAT_BITS_MAGICAL_MIGHT 0xC
-#define STAT_BITS_MAGICAL_MENDING 0x2
-#define STAT_BITS_PER_STAT 0xA
-
 struct PrimaryCombatStats {
-	unsigned short currHP;
-	unsigned short currMP;
-	unsigned short maxHP;
-	unsigned short maxMP;
-	unsigned short attack;
-	unsigned short defense;
-	unsigned short agility;
-	unsigned short unk;
-	int packedStats;
+    unsigned short currHP;
+    unsigned short currMP;
+    unsigned short maxHP;
+    unsigned short maxMP;
+    unsigned short attack;
+    unsigned short defense;
+    unsigned short agility;
+    unsigned short unk;
+    unsigned int charm : 10;
+    unsigned int magicalMight : 10;
+    unsigned int magicalMending : 10;
 };
 
 struct BaseCombatStats {
-	char unk[0x2C];
-	struct PrimaryCombatStats primaryStats;
+    char unk[0x2C];
+    struct PrimaryCombatStats primaryStats;
 };
 
 struct ModifiableCombatStats {
-	struct PrimaryCombatStats primaryStats;// 0xE
-	char unk1[0x44];
-	int buffPack;
+    struct PrimaryCombatStats primaryStats; // 0xE
+    char unk1[0x44];
+    signed int attackBuff : 3;
+    signed int defenseBuff : 3;
+    signed int agilityBuff : 3;
+    signed int charmBuff : 3;
+    signed int magicalMightBuff : 3;
+    signed int magicalMendingBuff : 3;
 };
 
 struct CombatantStruct {
-	unsigned short flags;
-	char unk[0x132];
-	struct BaseCombatStats* baseStats; // TODO: holds more general info than just stats
-	struct ModifiableCombatStats* currentStats; // includes things like buffs being applied
+    unsigned short flags;
+    char unk[0x132];
+    struct BaseCombatStats* baseStats; // TODO: holds more general info than just stats
+    struct ModifiableCombatStats* currentStats; // includes things like buffs being applied
 };
 
 struct BattleStruct {
-	int unk0;
-	int unk4;
-	struct CombatantStruct* combatantList[0xe9]; // TODO: validate this size as well as this struct as a whole
+    int unk0;
+    int unk4;
+    struct CombatantStruct* combatantList[0xe9]; // TODO: validate this size as well as this struct as a whole
 };
 struct BattleStruct* GetBattleStruct();
 struct CombatantStruct* GetCombatantFromList(struct BattleStruct* battleStruct, int id);
