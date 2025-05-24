dd613cf46cb4f657b90f94be5bc8e274e191c737
nat5_776 <deindall@outlook.com>
2025-05-24 20:24:22 +0200

    comment about random range bounds

--- include/System/Random.h
+++ include/System/Random.h
@@ -10,24 +10,24 @@ struct Random {
 
 #ifdef jpn
 // TODO: all jpn aliases
-#define NextRandomFloatBetween func_02075514
+    #define NextRandomFloatBetween func_02075514
 #endif
 
-/* generate a random unsigned integer between 0 and 0xFFFFFFFFu. */
+/* generate a random unsigned integer between 0 and 0xFFFFFFFFu (inclusive). */
 unsigned int NextRandom(struct Random* random);
 
-/* generate a random integer between 0 and maximum.
+/* generate a random integer between 0 and maximum (exclusive).
  * if maximum <= 0, returns 0 without updating state. */
 int NextRandomMax(struct Random* random, int maximum);
 
-/* generate a random floating point number between 0.0f and 1.0f. */
+/* generate a random floating point number between 0.0f and 1.0f (inclusive). */
 float NextRandomFloat01(struct Random* random);
 
-/* generate a random floating point number between minimum and maximum. */
+/* generate a random floating point number between minimum and maximum (inclusive). */
 float NextRandomFloatBetween(struct Random* random, float minimum, float maximum);
 
-/* generate a random floating point number between minimum and maximum with digits precision. */
+/* generate a random floating point number between minimum and maximum (inclusive) with digits precision. */
 float NextRandomFloatScaled(struct Random* random, float minimum, float maximum, int digits);
 
-/* generate a random integer between minimum and maximum */
+/* generate a random integer between minimum and maximum (inclusive) */
 int NextRandomBetween(struct Random* random, int minimum, int maximum);
