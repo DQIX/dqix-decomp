a2bb0c67291e292931d9f996de0c66658d9c95fd
eakeys <explodingarrowkeys@gmail.com>
2026-09-08 00:53:57 +0100

    transfer loot container rng scripting

--- src/Filesystem/FileIO.cpp
+++ src/Filesystem/FileIO.cpp
@@ -176,7 +176,7 @@ bool GetFileInNarc(const void *narcBuffer, const char *targetFilePath,
 }
 
 bool GetFileInNarcPermissive(const void *narcBuffer, const char *targetFilePath,
-    const void **pOutFilePtr, unsigned int *pOutFileSize, unsigned int firstFileIdx)
+    const void **pOutFilePtr, unsigned int *pOutFileSize)
 {
     bool success = false;
     LockResourceMutex();
