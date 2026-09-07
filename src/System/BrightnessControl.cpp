1a9a1924cd30eee7516b28afb632751b5526e63e
Jessy Grenon-Tremblay (ETS) <jessy.grenon.t@gmail.com>
2026-09-07 14:23:58 -0400

    func_0203b110 --> SetMainBrightness func_0203b19c --> SetSubBrightness func_0203b228 --> SetBrightness

--- src/System/BrightnessControl.cpp
+++ src/System/BrightnessControl.cpp
@@ -1,45 +1,89 @@
 #include "Combat/Main/UnknownContext.h"
+#include "System/Brightness.h"
+#include "System/BrightnessWrappers.h"
 #include <globaldefs.h>
 
-extern "C" ARM void func_0203b110(UnknownContext *context, int brightness, int duration);
-extern "C" ARM void func_0203b19c(UnknownContext *context, int brightness, int duration);
 extern "C" ARM void *func_020daf90();
 extern "C" ARM int func_020db9cc(void *unk, int screen, int brightness, unsigned int duration);
 
+
+extern "C" ARM void SetMainBrightness(UnknownContext *context, int brightness, int duration) {
+    if (context->mainBrightnessLocked != 0) return;
+
+    void *unk = func_020daf90();
+
+    if (func_020db9cc(unk, 0, brightness, duration) == 0) return;
+
+    if (duration == 0) {
+        context->mainBrightness              = (float) brightness;
+        context->mainBrightnessTarget        = brightness;
+        context->mainBrightnessTimeRemaining = 0;
+        context->mainBrightnessDirty         = 1;
+        return;
+    }
+
+    context->mainBrightnessTarget        = brightness;
+    context->mainBrightnessTimeRemaining = (int) ((float) duration * 16.66699981689453125f);
+}
+
+extern "C" ARM void SetSubBrightness(UnknownContext *context, int brightness, int duration) {
+    if (context->subBrightnessLocked != 0) return;
+
+    void *unk = func_020daf90();
+
+    if (func_020db9cc(unk, 1, brightness, duration) == 0) return;
+
+    if (duration == 0) {
+        context->subBrightness              = (float) brightness;
+        context->subBrightnessTarget        = brightness;
+        context->subBrightnessTimeRemaining = 0;
+        context->subBrightnessDirty         = 1;
+        return;
+    }
+
+    context->subBrightnessTarget        = brightness;
+    context->subBrightnessTimeRemaining = (int) ((float) duration * 16.66699981689453125f);
+}
+
+extern "C" ARM void SetBrightness(UnknownContext *context, int brightness, int duration) {
+    SetMainBrightness(context, brightness, duration);
+    SetSubBrightness(context, brightness, duration);
+}
+
 extern "C" ARM void SetAndLockMainBrightness(UnknownContext *context, int brightness, int duration) {
-    func_0203b110(context, brightness, duration);
+    SetMainBrightness(context, brightness, duration);
     context->mainBrightnessLocked = 1;
 }
 
 extern "C" ARM void SetAndLockSubBrightness(UnknownContext *context, int brightness, int duration) {
-    func_0203b19c(context, brightness, duration);
+    SetSubBrightness(context, brightness, duration);
     context->subBrightnessLocked = 1;
 }
 
 extern "C" ARM void SetAndLockBrightness(UnknownContext *context, int brightness, int duration) {
-    func_0203b110(context, brightness, duration);
+    SetMainBrightness(context, brightness, duration);
     context->mainBrightnessLocked = 1;
 
-    func_0203b19c(context, brightness, duration);
+    SetSubBrightness(context, brightness, duration);
     context->subBrightnessLocked = 1;
 }
 
 extern "C" ARM void UnlockAndSetMainBrightness(UnknownContext *context, int brightness, int duration) {
     context->mainBrightnessLocked = 0;
-    func_0203b110(context, brightness, duration);
+    SetMainBrightness(context, brightness, duration);
 }
 
 extern "C" ARM void UnlockAndSetSubBrightness(UnknownContext *context, int brightness, int duration) {
     context->subBrightnessLocked = 0;
-    func_0203b19c(context, brightness, duration);
+    SetSubBrightness(context, brightness, duration);
 }
 
 extern "C" ARM void UnlockAndSetBrightness(UnknownContext *context, int brightness, int duration) {
     context->mainBrightnessLocked = 0;
-    func_0203b110(context, brightness, duration);
+    SetMainBrightness(context, brightness, duration);
 
     context->subBrightnessLocked = 0;
-    func_0203b19c(context, brightness, duration);
+    SetSubBrightness(context, brightness, duration);
 }
 
 extern "C" ARM void SetMainBrightnessWithDurationMs(UnknownContext *context, int brightness, unsigned int durationMs) {
